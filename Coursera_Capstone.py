4675df7ec760b015bb4a93d651e8d573719b78c6
kilian100 <64424923+kilian100@users.noreply.github.com>
2020-04-27 20:37:51 +0200

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print( "Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
